c64ffaf119cdd7c4a78180be585002d41155de8c
ADIL SHAIK <adilshaik9112@gmail.com>
2026-04-04 21:54:27 +0530

    Fix: force correct Menu.js casing for Linux

--- src/components/menu.js
+++ src/components/menu.js
@@ -25,6 +25,6 @@ const Menu = ({ menuItems, filterItems }) => {
       </div>
     </div>
   );
-};
+};                                                                                                                                          
 
 export default Menu;
